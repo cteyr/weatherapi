024a6a1be67a735a9067f5380e8597c0afdf9098
cteyr123 <cteyr123@gmail.com>
2022-07-21 20:52:47 +0200

    useClimate hook refactoring

--- src/hooks/useClimate.ts
+++ src/hooks/useClimate.ts
@@ -1,36 +1,38 @@
+import { useState, useEffect } from "react";
+
 import { Climate } from "../types/Climate";
-import { useState,useEffect } from "react";
 import { api } from "../api/api";
-const useClimate=()=>{
-    const [Climate, setClimate] = useState<Climate>();
-    const [IsLoading, setIsLoading] = useState(false);
-    const [Latitud, setLatitud] = useState<number>(0);
-    const [Longitud, setLongitud] = useState<number>(0);
 
-    useEffect(()=>{
-      if (navigator.geolocation) { //check if geolocation is available
-        navigator.geolocation.getCurrentPosition(function(position){
-          setLatitud(position?.coords.latitude);
-          setLongitud(position?.coords.longitude);
-        });   
-        }
-    },[])
+const useClimate = () => {
+  const [Climate, setClimate] = useState<Climate>();
+  // const [IsLoading, setIsLoading] = useState(false);
+  const [Latitud, setLatitud] = useState<number>(0);
+  const [Longitud, setLongitud] = useState<number>(0);
 
-    const handleResponse = async () => {     
-      //setIsLoading(false);
-      const { response, error } = await api.getClimate(Latitud, Longitud);
-      if (error) {
-        console.log(error);
-      } else {
-        //setIsLoading(true);
-        setClimate(response);
-      }
-    };
-  
-    const getAllClimate = () => {
-      handleResponse();
-    };
-  
-    return { getAllClimate, Climate,Longitud,Latitud, setClimate };
-}
-export {useClimate}
+  useEffect(() => {
+    if (navigator.geolocation) {
+      //check if geolocation is available
+      navigator.geolocation.getCurrentPosition(function (position) {
+        setLatitud(position?.coords.latitude);
+        setLongitud(position?.coords.longitude);
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    if (Latitud && Longitud) {
+      api
+        .getClimate(Latitud, Longitud)
+        .then(({ response }) => {
+          setClimate(response);
+        })
+        .catch(({ error }) => {
+          console.error(error);
+        });
+    }
+  }, [Latitud, Longitud]);
+
+  return { Climate, Longitud, Latitud, setClimate };
+};
+
+export { useClimate };
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,6 +1,5 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "../components/Navbar";
-import { useEffect } from "react";
 import { useClimate } from "../hooks/useClimate";
 
 const MainLayout = () => {
