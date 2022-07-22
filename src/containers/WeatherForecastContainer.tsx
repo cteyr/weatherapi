54d856ce3dc1116a4e1252b105b05cce7baec130
cteyr123 <cteyr123@gmail.com>
2022-07-22 18:06:15 +0200

    main container design finish

--- src/containers/WeatherForecastContainer.tsx
+++ src/containers/WeatherForecastContainer.tsx
@@ -5,12 +5,16 @@ const WeatherForecastContainer = () => {
 
   return (
     <div className="WeatherForecastContainer">
-      <div className="color">{Climate?.location.region}</div>
-      <div className="color">sdas</div>
-      <div className="color">sdas</div>
-      <div className="color">sdas</div>
-      <div className="color">sdas</div>
-      <div className="color">sdas</div>
+      <div className="climate">
+        <div className="time-climate">
+          {Climate?.forecast.forecastday[0].hour[0].time}
+        </div>
+      </div>
+      <div className="climate"></div>
+      <div className="climate">sdas</div>
+      <div className="climate">sdas</div>
+      <div className="climate">sdas</div>
+      <div className="climate">sdas</div>
     </div>
   );
 };
